9ca09910ffb3ac26ea53e32389a2d8af0863b9be
nobody6669 <moisesalex132@gmail.com>
2021-12-03 22:40:17 -0600

    ➕ADD: database connection

--- dist/server.js
+++ dist/server.js
@@ -4,20 +4,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const cors_1 = __importDefault(require("cors"));
-const express_1 = __importDefault(require("express"));
 const morgan_1 = __importDefault(require("morgan"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const express_1 = __importDefault(require("express"));
+const mongoose_1 = __importDefault(require("mongoose"));
+// const Server = express();
 class Server {
     constructor() {
         this.app = (0, express_1.default)();
-        this.setConfig();
+        this.setConfigure();
+        this.setMongoConfig();
     }
-    setConfig() {
-        this.app.use(express_1.default.urlencoded({ extended: true }));
-        this.app.use(cors_1.default);
+    setConfigure() {
         this.app.use((0, morgan_1.default)("dev"));
+        this.app.use((0, cors_1.default)());
+        this.app.use(express_1.default.json());
+        this.app.use(express_1.default.urlencoded({ extended: true }));
         dotenv_1.default.config();
     }
+    setMongoConfig() {
+        mongoose_1.default.Promise = global.Promise;
+        mongoose_1.default.connect("mongodb://localhost/survey").then((db) => { console.log('data base connect'); });
+    }
 }
-exports.default = new Server().app;
+exports.default = Server;
 //# sourceMappingURL=server.js.map
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,33 @@
-import { Application } from 'express';
 import cors from 'cors';
-import express from 'express';
+import { Application } from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import express from 'express';
+import mongoose from 'mongoose';
 
+// const Server = express();
 class Server {
-
     public app: Application;
 
     constructor() {
         this.app = express();
-        this.setConfig();
+        this.setConfigure();
+        this.setMongoConfig();
     }
 
-    private setConfig() {
-        this.app.use(express.urlencoded({ extended: true }));
-        this.app.use(cors)
+    private setConfigure() {
         this.app.use(morgan("dev"));
+        this.app.use(cors());
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
         dotenv.config();
+    }
+
+    private setMongoConfig() {
+        mongoose.Promise = global.Promise;
+        mongoose.connect("mongodb://localhost/survey").then((db) => { console.log('data base connect'); })
     }
 }
 
-export default new Server().app;
+
+export default Server;
