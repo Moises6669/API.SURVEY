c0c5242f9077c88c7b446f0d56d986517731ae14
Moisés Masis <moisesalex132@gmail.com>
2022-03-12 23:51:08 -0600

    ➕ADD: verify email

--- src/controllers/API/user/signup.js
+++ src/controllers/API/user/signup.js
@@ -1,14 +1,20 @@
 const User = require("../../../models/user.models");
-
+const { Token } = require("../../../services/Tokens");
+const { getTemplate, sendEmail } = require("../../../services/Email");
 const signup = async (req, res) => {
   let body = req.body;
   try {
     const user = new User(body);
+    const token = new Token().generateToken(user, "5m");
+    const template = getTemplate(user.username, token);
+    sendEmail(user.email, "Confirme su Email", template);
     await user.save();
     if (user) {
       res.status(201).json({
         ok: true,
+        message: "Usuario Regisrado, confirme su email",
         user,
+        token,
       });
     }
   } catch (error) {
