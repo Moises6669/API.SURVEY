42437a2a6666fc2441a2ab495d143baf25600ed0
RochaMoran <rochamoran1@gmail.com>
2022-04-06 18:47:29 +0200

    ADD: Controller getOneSurvey

--- src/controllers/API/routes/survey.js
+++ src/controllers/API/routes/survey.js
@@ -2,11 +2,12 @@ const express = require("express");
 const routes = express.Router();
 
 const { postSurvey } = require('../survey/createSurvey')
-const { getAllSurveys } = require('../survey/getSurveys')
+const { getAllSurveys, getOneSurveys } = require('../survey/getSurveys')
 const { answerSurvey } = require('../survey/answerSurvey')
 
 routes.post("/survey/create", postSurvey);
 routes.post("/survey/answer/:id", answerSurvey);
 routes.get("/surveys", getAllSurveys);
+routes.get("/surveys/:id", getOneSurveys);
 
 module.exports = routes;
--- src/controllers/API/survey/getSurveys.js
+++ src/controllers/API/survey/getSurveys.js
@@ -19,3 +19,24 @@ exports.getAllSurveys = (req,res) => {
         });
       });
 }
+
+exports.getOneSurveys = (req,res) => {
+    const {id} = req.params;
+    surveyModel.findOne({_id: id}, (err, surveys) => {
+        if (err)
+          return res.status(500).json({
+            message: "Error inesperado al obtener esta encuesta 🧾🤦‍♂️",
+            err
+          });
+
+        if (!surveys)
+          return res.status(404).json({
+            message: "Esta encuesta no existe 💔😯"
+          });
+    
+        res.status(200).json({
+          ok:true,
+          survey:surveys
+        });
+      });
+}
