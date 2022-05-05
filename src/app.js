76c00c640d46df2bb8f1b6c6887b08fd90d69d51
Moisés Masis <moisesalex132@gmail.com>
2022-05-05 06:08:40 -0600

    first deploy

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
 const app = require('./server');
 app.listen(process.env.PORT,()=>{
-    console.log(`Server on Port ${process.env.PORT}`);
+    console.log(`Server on port ${process.env.PORT}`);
 });
